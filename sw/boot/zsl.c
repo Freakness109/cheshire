543faee5abfa2ab7673c2374ff3b298c2d9650e8
Axel Vanoni <axvanoni@student.ethz.ch>
2026-02-08 14:39:11 +0100

    Integrate SD boot into zsl

--- sw/boot/zsl.c
+++ sw/boot/zsl.c
@@ -14,6 +14,8 @@
 #include "gpt.h"
 #include "dif/uart.h"
 #include "printf.h"
+#include "sdhcvar.h"
+#include "sdmmcvar.h"
 
 // Type for firmware payload
 typedef int (*payload_t)(uint64_t, uint64_t, uint64_t);
@@ -21,6 +23,8 @@ typedef int (*payload_t)(uint64_t, uint64_t, uint64_t);
 // Static variables to wrap relocated reads
 static gpt_read_t rread;
 static void *rgp;
+
+static uint8_t scratch_buffer_sd[512] = {0};
 
 // Call foreign read procedure with another global pointer
 static inline int grread(void *priv, void *buf, uint64_t addr, uint64_t len) {
@@ -31,23 +35,68 @@ static inline int grread(void *priv, void *buf, uint64_t addr, uint64_t len) {
 }
 
 static inline void load_part_or_spin(void *priv, const uint64_t *pguid, void *const dst,
-                                     const char *name, uint64_t max_lbas) {
+                                     const char *name, uint64_t max_lbas, gpt_read_t read_func) {
     uint64_t lba_begin, lba_end;
     int64_t part_idx = -1;
-    if (gpt_find_partition(grread, priv, &part_idx, &lba_begin, &lba_end, max_lbas, pguid, 0))
+    int ret = 0;
+    if (gpt_find_partition(read_func, priv, &part_idx, &lba_begin, &lba_end, max_lbas, pguid, 0))
         printf("[ZSL] Error finding %s", name);
     else if (part_idx < 0)
         printf("[ZSL] No %s", name);
     else {
         printf("[ZSL] Copy %s (part %d, LBA %d-%d) to 0x%lx... ", name, part_idx, lba_begin,
                lba_end, dst);
-        grread(priv, dst, 0x200 * lba_begin, 0x200 * (lba_end - lba_begin + 1));
-        printf("OK\r\n");
-        return;
+        ret = read_func(priv, dst, 0x200 * lba_begin, 0x200 * (lba_end - lba_begin + 1));
+        if (ret == 0) {
+            printf("OK\r\n");
+            return;
+        }
     }
     // Catch
     printf(" with at most %d sectors and type GUID 0x%llx%llx", max_lbas, pguid[1], pguid[0]);
     while (1) wfi();
+}
+
+static inline int sd_grread(void *priv, void *buf, uint64_t addr, uint64_t len) {
+    struct sdmmc_softc *sc = priv;
+    size_t max_read_size = 0x040000;
+
+    int ret = 0;
+    // optimizer hint
+    if (priv == buf)
+        return 1;
+
+    if (addr & 0x1FF) {
+        ret = sdmmc_mem_read_block(&sc->sc_card, (addr / 0x200), scratch_buffer_sd, 512);
+        if (ret) {
+            printf("First read (0x%x bytes @ Block 0x%x), ret=%d\r\n", 512, (addr / 0x200), ret);
+            return ret;
+        }
+        for (size_t i = addr & 0x1FF; i < 512 && len > 0; ++i) {
+            *(uint8_t*)buf = scratch_buffer_sd[i];
+            ++buf;
+            --len;
+        }
+    }
+
+    while (len >= max_read_size) {
+        ret = sdmmc_mem_read_block(&sc->sc_card, ((addr+0x1FF) / 0x200), buf, max_read_size);
+        if (ret) {
+            printf("Loop read (0x%x bytes @ Block 0x%x), ret=%d\r\n", max_read_size, ((addr+0x1FF) / 0x200), ret);
+            return ret;
+        }
+        len -= max_read_size;
+        addr += max_read_size;
+        buf += max_read_size;
+    }
+
+    if (len == 0)
+        return ret;
+
+    ret = sdmmc_mem_read_block(&sc->sc_card, ((addr+0x1FF) / 0x200), buf, len);
+    if (ret)
+        printf("Last read (0x%x bytes @ Block 0x%x), ret=%d\r\n", len, ((addr+0x1FF) / 0x200), ret);
+    return ret;
 }
 
 int main(void) {
@@ -74,10 +123,24 @@ int main(void) {
            bootmode, rtc_freq, core_freq, rgp, read, priv);
 
     // If this is a GPT disk boot, load payload and device tree
-    if (read & 1) {
+    if (bootmode == 1) {
+        struct sdmmc_softc sc = { 0 };
+        struct sdhc_host hp = { 0 };
+        sdhc_init(&hp, 0x01001000, 0, 0);
+        sdmmc_init(&sc, &hp, scratch_buffer_sd);
+        sdhc_bus_clock(sc.sch, SDMMC_SDCLK_25MHZ, SDMMC_TIMING_LEGACY);
+        if (!ISSET(sc.sc_flags, SMF_CARD_ATTACHED)) {
+            printf("Failed to initialize SD Card\n");
+            uart_write_flush(&__base_uart);
+            return 1;
+        }
+        priv = &sc;
+        load_part_or_spin(priv, __BOOT_DTB_TYPE_GUID, __BOOT_ZSL_DTB, "device tree", 64, sd_grread);
+        load_part_or_spin(priv, __BOOT_FW_TYPE_GUID, __BOOT_ZSL_FW, "firmware", 8192, sd_grread);
+    } else if (read & 1) {
         rread = (gpt_read_t)(void *)(uintptr_t)(read & ~1);
-        load_part_or_spin(priv, __BOOT_DTB_TYPE_GUID, __BOOT_ZSL_DTB, "device tree", 64);
-        load_part_or_spin(priv, __BOOT_FW_TYPE_GUID, __BOOT_ZSL_FW, "firmware", 8192);
+        load_part_or_spin(priv, __BOOT_DTB_TYPE_GUID, __BOOT_ZSL_DTB, "device tree", 64, grread);
+        load_part_or_spin(priv, __BOOT_FW_TYPE_GUID, __BOOT_ZSL_FW, "firmware", 8192, grread);
     }
 
     // Launch payload
