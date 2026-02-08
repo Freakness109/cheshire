76abf529b6ac7c6f957a0772cb4dc3ed2ecd2d67
Axel Vanoni <axvanoni@student.ethz.ch>
2026-02-08 14:40:13 +0100

    xilinx.mk: Add missing dependency on Bender.lock

--- target/xilinx/xilinx.mk
+++ target/xilinx/xilinx.mk
@@ -53,7 +53,7 @@ CHS_XILINX_IPS_genesys2 := clkwiz vio mig7s
 CHS_XILINX_IPS_vcu128   := clkwiz vio ddr4
 CHS_XILINX_IPS_vcu118   := clkwiz vio ddr4
 
-$(CHS_XILINX_DIR)/scripts/add_sources.%.tcl: $(CHS_ROOT)/Bender.yml $(CHS_XILINX_HW)
+$(CHS_XILINX_DIR)/scripts/add_sources.%.tcl: $(CHS_ROOT)/Bender.yml $(CHS_ROOT)/Bender.lock $(CHS_XILINX_HW)
 	$(BENDER) script vivado -t fpga -t $* $(CHS_BENDER_RTL_FLAGS) > $@
 
 define chs_xilinx_bit_rule
